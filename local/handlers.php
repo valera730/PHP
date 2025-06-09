4603f3ec167d5a088916a614b9bd38e36e506a2d
Valera730 <telmob@yandex.ru>
2025-06-09 10:02:13 +0300

    rusEnding

--- local/handlers.php
+++ local/handlers.php
@@ -328,3 +328,11 @@ function ResizeElementProperty(&$arFields) {
         DeleteDirFilesEx('/upload/claims/files/');
     }
 }
+
+function rusEnding($n, $n1, $n2, $n5) {
+    if($n >= 11 and $n <= 19) return $n5;
+    $n = $n % 10;
+    if($n == 1) return $n1;
+    if($n >= 2 and $n <= 4) return $n2;
+    return $n5;
+}
