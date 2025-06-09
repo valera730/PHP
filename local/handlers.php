a61abada574d4d432b52bc954b848b1020fe2fda
Valera730 <telmob@yandex.ru>
2025-06-09 09:48:36 +0300

    FilterOnBeforeAddUpdate

---
+++ local/handlers.php
@@ -0,0 +1,77 @@
+<?
+use Bitrix\Main\EventManager;
+use Bitrix\Main\Loader;
+use Bitrix\Sale\DiscountCouponsManager;
+use Bitrix\Main\Mail\Event;
+use Bitrix\Sale;
+use Bitrix\Sale\EntityMarker;
+use Bitrix\Main\Type\DateTime;
+use Bitrix\Highloadblock as HL;
+use Bitrix\Main\Entity;
+use Bitrix\Main\Application;
+use Lkduslar\Core\Rest\Yadisk;
+use Lkduslar\Core\Wsdl1c;
+use Lkduslar\Core\Main;
+use Bitrix\Highloadblock\HighloadBlockTable;
+
+EventManager::getInstance()->addEventHandler('', 'KitchenFillingFilterOnBeforeUpdate', 'FilterOnBeforeAddUpdate');
+EventManager::getInstance()->addEventHandler('', 'KitchenFillingFilterOnBeforeAdd', 'FilterOnBeforeAddUpdate');
+EventManager::getInstance()->addEventHandler('', 'CabinetFillingFilterOnBeforeUpdate', 'FilterOnBeforeAddUpdate');
+EventManager::getInstance()->addEventHandler('', 'CabinetFillingFilterOnBeforeAdd', 'FilterOnBeforeAddUpdate');
+
+function FilterOnBeforeAddUpdate(\Bitrix\Main\Entity\Event $event) {
+    $arFields = $event->getParameter('fields');
+
+    $baseProps = [];
+    if ($arFields['UF_BASE_PROPS'] && is_array($arFields['UF_BASE_PROPS'])) {
+        foreach ($arFields['UF_BASE_PROPS'] as $key => $val) {
+            TrimArr($val);
+
+            if(preg_match('/VALS_([0-9]+)/is', $key, $m)) {
+                if(!in_array($m[1], $val['PROPS'])) {
+                    unset($arFields['UF_BASE_PROPS'][$key]);
+                }
+            }
+
+            if(empty($val)) {
+                unset($arFields['UF_BASE_PROPS'][$key]);
+            }
+        }
+
+        $arFields['UF_BASE_PROPS'] = serialize($arFields['UF_BASE_PROPS']);
+    }
+
+    if ($arFields['UF_USER_PROPS'] && is_array($arFields['UF_USER_PROPS'])) {
+        foreach ($arFields['UF_USER_PROPS'] as $key => $val) {
+            TrimArr($val);
+
+            if(preg_match('/VALS_([0-9]+)/is', $key, $m)) {
+                if(!in_array($m[1], $val['PROPS'])) {
+                    unset($arFields['UF_USER_PROPS'][$key]);
+                }
+            }
+
+            if ($val == 'N') {
+                unset($arFields['UF_USER_PROPS'][$key]);
+            }
+
+            if(empty($val)) {
+                unset($arFields['UF_USER_PROPS'][$key]);
+            }
+        }
+
+        $arFields['UF_USER_PROPS'] = serialize($arFields['UF_USER_PROPS']);
+    }
+
+    if ($arFields['UF_USER_PROPS'] == "N") {
+        $arFields['UF_USER_PROPS'] = "";
+    }
+
+    $result = new \Bitrix\Main\Entity\EventResult();
+
+    $event->setParameter("fields", $arFields);
+
+    $result->modifyFields($arFields);
+
+    return $result;
+}
