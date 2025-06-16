466a4dc4f4ed063cd839b1575d66d31fbd2ef9ab
Valera730 <telmob@yandex.ru>
2025-06-16 17:53:14 +0300

    B24CrmContragentsOnBeforeUpdate

---
+++ bitrix/php_interface/include/events/EventsB24CrmContragents.php
@@ -0,0 +1,53 @@
+<?php
+$eventManager = \Bitrix\Main\EventManager::getInstance();
+$eventManager->addEventHandler('', 'B24CrmContragentsOnBeforeUpdate', [EventsB24CrmContragents::class, 'OnBeforeUpdate']);
+
+class EventsB24CrmContragents {
+    function OnBeforeUpdate(\Bitrix\Main\Entity\Event $event) {
+    	$result = new \Bitrix\Main\Entity\EventResult;
+
+        $id = $event->getParameter("id");
+        $id = $id["ID"];
+
+        $entity = $event->getEntity();
+        $entityDataClass = $entity->GetDataClass();
+
+        $arFields = $event->getParameter("fields");
+
+        $item = $entityDataClass::getByPrimary($id)->fetch();
+
+        $initiator = 46;
+
+        if(defined("B24_INITIATOR_B24"))
+            $initiator = 47;
+
+        if(defined("B24_INITIATOR_1С"))
+            $initiator = 48;
+
+        $rsData = \Bitrix\Highloadblock\HighloadBlockTable::getById(43);
+
+        if ($arData = $rsData->fetch()) {
+            $entityLog = \Bitrix\Highloadblock\HighloadBlockTable::compileEntity($arData);
+            $DataClassLog = $entityLog->getDataClass();
+
+            foreach ($arFields as $key => $value) {
+                if(in_array($key, ['UF_UPDATE'])) continue;
+
+                if($value != $item[$key]) {
+                    $DataClassLog::add([
+                        'UF_INTIATOR' => $initiator,
+                        'UF_TIMESTAMP' => new \Bitrix\Main\Type\DateTime(),
+                        'UF_TYPE' => 45,
+                        'UF_CRM_ID' => $id,
+                        'UF_B24_ID' => $item['UF_ENTITY_ID'],
+                        'UF_FIELD' => $key,
+                        'UF_OLD_VALUE' => $item[$key],
+                        'UF_NEW_VALUE' => $value
+                    ]);
+                }
+            }
+        }
+
+    	return $result;
+    }
+}
